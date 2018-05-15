e05bd168709bd2322aca244f6a0d980097393f08
Giovani Britton <gcbrittono@unal.edu.co>
2018-05-14 23:03:50 -0500

    Adicion de algunas funciones al cliente paa hacer varios procesos

--- chatClient.c
+++ chatClient.c
@@ -12,14 +12,95 @@
 #include <string.h>
 #include <ctype.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define PORT 3535
 #define MSGSIZE 32
 #define STRUCTSIZE sizeof(struct DogType)
 
+//Definimos constantes para cuadrar el numero de clientes/hilos que se utilizaran al tiempo
+#define NUM_CLIENTES 5 
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+//  Definición de la función de los hilos	
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+void *connection_handler(void *threadid){
+	int threadnum = (intptr_t)threadid;
+	int clientfd, r;
+	char msg[32];
+
+	//Esta estructura gurada la información concerniente al servidor con el cual el cliente quiere conectarse
+	struct sockaddr_in serv_addr;
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+//  Creacion del socket() clientfd	
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+	clientfd = socket(AF_INET,SOCK_STREAM,0);
+	//validar
+	if(clientfd==-1){
+		perror("Error en socket del clientfd\n");
+		exit(-1);
+	}	
+	serv_addr.sin_family= AF_INET;
+	serv_addr.sin_port = htons(PORT);
+	serv_addr.sin_addr.s_addr= inet_addr("127.0.0.1");
+	bzero(serv_addr.sin_zero,8);
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+// Se conectan el cliente y el servidor
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+
+	r=connect(clientfd,(struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	//validar
+	if(r<0){
+		perror("Error en connect de r\n");
+		exit(-1);
+	}
+
+
+	printf("Conexión exitosa cliente número:%d\n ", threadnum);
+	while(1){
+		printf("Conexion con el cliente: %d\n", threadnum);
+		printf("Escribir mensaje");
+		scanf("%s",msg);	
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+// Envio de informacion
+//-----------------------------------------------------------------------------------------------------------------------------------------
+		r = send(clientfd, msg, MSGSIZE,0);
+		if(r == -1){
+			perror("Error en send");
+			exit(-1);	
+		}
+	
+	sleep(2);
+	}
+
+}
+
+
+
 
 int main(){
+	int clientfd, new_socket, c, *new_sock, i;
+	pthread_t cliente_hilo;
+	for(i = 1; i <= NUM_CLIENTES; i++){
+		if(pthread_create(&cliente_hilo, NULL, connection_handler, (void*)&i)<0){
+		perror("No se pudo crear el hilo");
+		exit(-1);	
+		}
+	}
+	pthread_exit(NULL);
 
+
+/*
 	int clientfd,r;
 	char msg[32];
 
@@ -82,7 +163,7 @@ int main(){
 		}
 	}
 
-
+*/
 	close(clientfd);
 	return 0;
 }
